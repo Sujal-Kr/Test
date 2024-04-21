575b01bed17c845341f47102337e4284faa5ec53
Sujal-Kr <85947816+Sujal-Kr@users.noreply.github.com>
2024-04-21 11:34:36 +0530

    Fixed Login issue

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -26,35 +26,35 @@ async function signup(req, res) {
 }
 
 async function login(req, res) {
-    try{
-        const {email,password}=req.body
-        
-        const user=await userModel.findOne({email:email})
-        if(user){
-            if(bcrypt.compare(password,user.password)){
-                let id=user._id
-                let token = jwt.sign({ payload: id }, KEY)
-                res.cookie('login', token, { httpOnly: true })
-                res.json({
-                    message: "user logged in successfully",
-                    user:user
-                })
-            }else{
-                res.json({
-                    message: "Wrong credentials try again",
-                })
+    try {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email: email });
+        if (user) {
+            const passwordMatch = await bcrypt.compare(password, user.password);
+            if (passwordMatch) {
+                let id = user._id;
+                let token = jwt.sign({ payload: id }, KEY);
+                res.cookie('login', token, { httpOnly: true });
+                return res.json({
+                    message: "User logged in successfully",
+                    user: user
+                });
+            } else {
+                return res.status(401).json({
+                    message: "Wrong credentials. Please try again."
+                });
             }
-            
-        }else{
-            res.json({
-                message: "User not found. Please register with us",
-            })
+        } else {
+            return res.status(404).json({
+                message: "User not found. Please register with us."
+            });
         }
-    }catch(err){
-        res.status(500).json({message: err.message})
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
-    
 }
+
 
 function logout(){
     res.cookie("login",'',{maxAge:1})
